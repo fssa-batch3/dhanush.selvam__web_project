4effdd0479a2bd75dc43f37c1765e47ba1b687ae
DhanushS212003 <dhanush.selvam@fwsa.freshworks.com>
2023-01-08 19:24:54 +0530

    edit css files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const link_container = document.getElementsByClassName('link_container')[0]
+const navbarLinks = document.getElementsByClassName('navbar')[0]
 
 toggleButton.addEventListener('click', () => {
-    link_container.classlist.toggle('active')
+    navbarLinks.classlist.toggle('active')
 })
